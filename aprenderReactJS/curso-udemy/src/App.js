7e8343131255652fc9d192cbde7ef4c15c3b2bc0
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-20 14:40:08 -0300

    Acercamiento al uso de componentes

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,12 +1,26 @@
+import React, {Component}  from 'react'
 import logo from './logo.svg';
 import './App.css';
+
 //No es HMTL es JSX, sintaxis XML que se preprocesa y se convierte en JS
+/*function Intro(props){
+  return <h1>{props.title}</h1>
+}*/
+
+/*const Intro = (props) => <h2> {props.title} </h2>*/
+
+class Intro extends Component{
+  render(){
+    return <h1>{this.props.title}</h1>
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Introducción a React.js</h1>
+        <Intro title = "Intro desde Props" />
         <a
           className="App-link"
           href="https://reactjs.org"
