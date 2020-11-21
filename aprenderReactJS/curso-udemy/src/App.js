2ae90b7e058f2235814a964a5351cd33bd95699b
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 18:43:09 -0300

    Event onClick

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from "react";
-import cars from './data/cars.json'
 
-class CarItem extends Component{
-    render(){
-        const {car, id} = this.props
-        return(
-            <li key = {car.id}>
-                <p><strong>Key: </strong>{id}</p>
-                <p><strong>Nombre: </strong>{car.name}</p>
-                <p><strong>Marca: </strong>{car.company}</p>
-            </li>
-            )
-    }
-}
+
 class App extends Component{
     render() {
         //Cada descendiente del array debe tener una unique key
         return (
             <div className='App'>
-                <h4>Trabajando con Objetos</h4>
-                <ul>
-                    {
-                        cars.map(car=>{
-                            return <CarItem key= {car.id} id={car.id} car={car}/>
-                        })
-                    }
-                </ul>
+                <h4>Trabajando con Eventos</h4>
+                <button onClick={()=>alert('Me clickeaste')}> 
+                Clickeame
+                </button>
             </div>
         )
     }
