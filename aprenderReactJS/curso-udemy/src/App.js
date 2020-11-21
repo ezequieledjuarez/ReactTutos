adb01f0db2598682d34aa77de8c3f2dea3df8157
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 16:48:03 -0300

    Trabajando con objetos

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
-import ConditionalSection from './sections/Conditional.js'
+import cars from './data/cars.json'
 
-
+class CarItem extends Component{
+    render(){
+        const {car, id} = this.props
+        return(
+            <li key = {car.id}>
+                <p><strong>Key: </strong>{id}</p>
+                <p><strong>Nombre: </strong>{car.name}</p>
+                <p><strong>Marca: </strong>{car.company}</p>
+            </li>
+            )
+    }
+}
 class App extends Component{
     render() {
+        //Cada descendiente del array debe tener una unique key
         return (
             <div className='App'>
-                <ConditionalSection/>
+                <h4>Trabajando con Objetos</h4>
+                <ul>
+                    {
+                        cars.map(car=>{
+                            return <CarItem key= {car.id} id={car.id} car={car}/>
+                        })
+                    }
+                </ul>
             </div>
         )
     }
