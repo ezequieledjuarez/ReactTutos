e96f5316e677b180a79ce94848c60b82e7e41f08
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 19:29:58 -0300

    Referencias

--- aprenderReactJS/curso-udemy/src/sections/Forms.js
+++ aprenderReactJS/curso-udemy/src/sections/Forms.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
 export default class Forms extends Component{
-    handleClick(e){
+    handleClick =(e)=>{
         e.preventDefault()
-        const name = document.getElementById('name').value
-        const email = document.getElementById('twitter').value
+        const name = this.inputName.value
+        const email = this.inputTwitter.value
         console.log({name,email})
     }
     render(){
@@ -17,14 +17,16 @@ export default class Forms extends Component{
                     <input 
                         id='name'
                         name='userName'
-                        placeholder = 'Introduce tu nombre'/>
+                        placeholder = 'Introduce tu nombre'
+                        ref={inputElement=>this.inputName=inputElement}/>
                 </p>
                 <p>
                 <label htmlFor='twitter'>Twitter: </label>
                 <input
                     id='twitter'
                     name='twitterAccount'
-                    placeholder='Introduce tu Twitter'/>
+                    placeholder='Introduce tu Twitter'
+                    ref={inputElement=>this.inputTwitter=inputElement}/>
                 </p>
                 <button onClick={this.handleClick}>Enviar</button>    
             </form>
