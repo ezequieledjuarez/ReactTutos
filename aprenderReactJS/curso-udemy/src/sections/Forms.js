f165b745eb4badc66c17b6652c3c6226480d6d9b
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 19:46:56 -0300

    Componentes controlados

--- aprenderReactJS/curso-udemy/src/sections/Forms.js
+++ aprenderReactJS/curso-udemy/src/sections/Forms.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Forms extends Component{
+    constructor(){
+        super()
+        this.state={
+            inputName:'',
+            inputTwitter:'@',
+            inputTerms:true
+        }
+    }
     handleSubmit =(e)=>{
         e.preventDefault()
-        const name = this.inputName.value
-        const email = this.inputTwitter.value
-        console.log({name,email})
+        console.log(this.state)
     }
-    handleChange(e){
+    handleChange = (e) =>{
         console.log('handleChange')
         console.log(e.target.checked)
+        this.setState({inputTerms: e.target.checked})
     }
     render(){
         return(
@@ -21,21 +28,26 @@ export default class Forms extends Component{
                     <input 
                         id='name'
                         name='userName'
+                        onChange={e=>this.setState({inputName: e.target.value})}
                         placeholder = 'Introduce tu nombre'
-                        ref={inputElement=>this.inputName=inputElement}/>
+                        ref={inputElement=>this.inputName=inputElement}
+                        value={this.state.inputName}/>
                 </p>
                 <p>
                 <label htmlFor='twitter'>Twitter: </label>
                 <input
                     id='twitter'
                     name='twitterAccount'
+                    onChange = {e=>this.setState({inputTwitter: e.target.value })}
                     placeholder='Introduce tu Twitter'
-                    ref={inputElement=>this.inputTwitter=inputElement}/>
+                    ref={inputElement=>this.inputTwitter=inputElement}
+                    value={this.state.inputTwitter}/>
                 </p>
                 <label>
                     <input 
                         onChange={this.handleChange}
-                        type='checkbox'/>    
+                        type='checkbox'
+                        checked={this.state.inputTerms}/>    
                         Accepted Terms
                 </label>    
                 <button>Enviar</button>    
