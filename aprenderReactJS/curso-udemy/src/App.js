2ab70abb6cbe80549508b04ba634d2b768743630
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 16:07:20 -0300

    Renderizado condicional

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from "react";
-// Las clases deben ir en mayus
-class Contador extends Component{
+import ConditionalSection from './sections/Conditional.js'
 
-        constructor(props){
-            super(props)
-            this.state = {contador: this.props.contadorInicial}
-        
-            setInterval(()=> {
-            this.setState({contador: this.state.contador +1 })
-        }, 1000)
-        }
-        
-        render(){
-        
-        return <ContadorNumero numero={this.state.contador}/> 
-        }
-    }
-    Contador.defaultProps = {
-        contadorInicial : 0
-    }
 
-class ContadorNumero extends Component{
+class App extends Component{
     render() {
-        console.log('ContadorNumero render()')
         return (
-            <div>
-                <span> {this.props.numero} </span>
+            <div className='App'>
+                <ConditionalSection/>
             </div>
         )
     }
 }
-    
-    class App extends Component{
-        render() {
-            return (
-                <div className = "App">
-               <p> Propagando el state de nuestros componentes</p>
-               <Contador contadorInicial = {100}/>
-                </div>
-            )
-        }
-    }
-    
-
 export default App;
