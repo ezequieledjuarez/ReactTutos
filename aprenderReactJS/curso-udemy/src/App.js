fa559dc1a631b3210af09f8c27ea75ee8558576f
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 18:44:58 -0300

    Eventos sintéticos

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 
 
 class App extends Component{
+    handleClick(e){
+        console.log(e)
+        console.log(e.nativeEvent)
+        alert('Me clickeaste')
+    }
     render() {
         //Cada descendiente del array debe tener una unique key
         return (
             <div className='App'>
                 <h4>Trabajando con Eventos</h4>
-                <button onClick={()=>alert('Me clickeaste')}> 
+                <button onClick={this.handleClick}> 
                 Clickeame
                 </button>
             </div>
