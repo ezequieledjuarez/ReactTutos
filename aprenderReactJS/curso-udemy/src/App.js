8fde16ca37e9aa192447759260b471782bd5e5ba
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 20:03:12 -0300

    Props children

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
-import Forms from './sections/Forms.js'
 
+class Box extends Component{
+    render() {
+        return (
+            <div style={{
+                border: '1px solid #000',
+                margin:5,
+                padding:5}}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class Article extends Component{
+    render() {
+        return (
+            <div>
+                <section>
+                    <h2>{this.props.title}</h2>    
+                    <p><em>Escrito por {this.props.author}</em></p>
+                    <Box>{this.props.date}</Box>        
+                    <article>
+                        {this.props.children}
+                    </article>    
+                </section>    
+            </div>
+        )
+    }
+}
 class App extends Component{
     render() {
         //Cada descendiente del array debe tener una unique key
         return (
             <div className='App'>
-                <h4>Trabajando con Forms</h4>
-                <Forms/>    
+                <h4>Trabajando con Children props</h4>
+                <Article
+                    author='EzzeGuason'
+                    date={new Date().toLocaleDateString()}
+                    title='Articulo sobre la propiedad Children'
+                >
+                    <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+                    <strong>Y mantiene las propiedades y componentes que hay añadidos dentro</strong>        
+                </Article>      
             </div>
         )
     }
