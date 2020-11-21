1e34d0e7966471bfbcc0cf6c3b1ccc6b6c83839f
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-21 18:55:39 -0300

    Enlace de contexto y mouseMove

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 
 
 class App extends Component{
+    constructor(){
+        super()
+        this.state = {mouseX:0, mouseY:0}
+    }
     handleClick(e){
         console.log(e)
         console.log(e.nativeEvent)
         alert('Me clickeaste')
+    }
+//Las arrow functions enlazan siempre el contexto dónde se declaran
+    handleMouseMove = (e)=>{
+        const{clientX,clientY} = e
+        this.setState({mouseX: clientX, mouseY:clientY})
     }
     render() {
         //Cada descendiente del array debe tener una unique key
@@ -15,6 +24,16 @@ class App extends Component{
                 <button onClick={this.handleClick}> 
                 Clickeame
                 </button>
+                <div
+                    onMouseMove={this.handleMouseMove}
+                    style={{
+                        border: '1px solid #000',
+                        marginTop: 10,
+                        padding:10
+                    }}
+                >
+                    <p>{this.state.mouseX},{this.state.mouseY}</p>
+                </div>
             </div>
         )
     }
