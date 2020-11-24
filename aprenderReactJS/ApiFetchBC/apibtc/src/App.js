1549c0030b5f52f5d70497a8a75a877b4908a125
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-24 14:42:27 -0300

    Consulta precios de BTC a través de una API

---
+++ aprenderReactJS/ApiFetchBC/apibtc/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Fetch from'./sections/Fetch.js'
+
+function App() {
+  return (
+    <div className = "App">
+       <Fetch/>
+    </div>
+  );
+}
+
+export default App;
