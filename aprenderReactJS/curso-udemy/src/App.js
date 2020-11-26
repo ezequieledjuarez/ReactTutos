862b684079ab274a3218467ecf369c51a4b371dd
EzzeGuason <ezze.juarezz@gmail.com>
2020-11-26 08:09:43 -0300

    Herencia vs Composicion 1

--- aprenderReactJS/curso-udemy/src/App.js
+++ aprenderReactJS/curso-udemy/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
-import ComponentDidCatch from './sections/life-cycle/ComponentDidCatch.js'
+
+class Button extends Component{
+    constuctor(props){
+        super()
+        this.borderColor ='#09f'
+    }
+    render(){
+        return(
+            <button style={{borderColor: this.borderColor, display: 'block'}}>
+                {this.props.label}
+            </button>
+        )
+        
+    }
+}
+    class ButtonDanger extends Button{
+        constructor(props){
+            super()
+            this.borderColor = 'red'
+        }
+    }
+
+    class buttonWithLegend extends Button{
+        render(){
+            <div>
+                {super.render()}
+                <small>{this.props.legend}</small>
+            </div>
+        }
+    }
+
 class App extends Component{
     render() {
         
         return (
             <div className='App'>
-            <ComponentDidCatch />   
+                <h4>Composicion vs Herencia</h4>  
+                <Button label='Click aqui'/>
+                <br />
+                <ButtonDanger label='no Click aqui'/>
+                <br/>
+                <buttonWithLegend
+                    label = 'Boton con explicacion'
+                    legend = 'Clickea para hacer algo'/>
             </div>
         )
     }
