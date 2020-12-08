ccc282045d721189cd1506685bb1c133a78c290f
ezequieledjuarez <ezequieledjuarez@gmail.com>
2020-12-08 20:16:38 -0300

    Configuraciones en index.js

--- 4Proyectos/personas/src/index.js
+++ 4Proyectos/personas/src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import createHistory from 'history/createBrowserHistory'
+import {routerMiddleware} from 'react-router-redux' //Conecta redux con router
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+
+const history = createHistory()
+const middleware = [ routerMiddleware(history), thunk] 
+
+const store = createStore(
+  reducers,
+  applyMiddleware(...middleware)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //El provider se encarga de conectar react & redux, se le debe pasar una store
+  <Provider store = {store}>
+     <App />
+  </Provider>,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
