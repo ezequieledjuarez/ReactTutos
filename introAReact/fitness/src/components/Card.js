211f6d7d7c187fad7a8be1dd445734e239d3bc25
EzeJuarez <ezequieledjuarez@gmail.com>
2021-01-07 18:26:43 -0300

    HardCoding the state

--- introAReact/fitness/src/components/Card.js
+++ introAReact/fitness/src/components/Card.js
@@ -4,6 +4,22 @@ import imageCircle from '../images/circles.png'
 import './styles/Card.css'
 
 class Card extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                image: imageExcercise
+            })
+        }, 5000);
+    }
+
     render(){
         const {description, img, leftColor, rightColor, title} = this.props
     return (
@@ -18,7 +34,7 @@ class Card extends React.Component{
                      <div className="col-6">
                         <img
                             className="float-right" 
-                            src={img}/>
+                            src={this.state.image}/>
                     </div>
                     <div className="col-6 Fitness-Card-Info">
                         <h1>{title}</h1>
