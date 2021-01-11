98cae1dd1459935b7bb3c01e5984a5b571a786ed
EzzeGuason <ezequieledjuarez>
2021-01-11 20:21:43 -0300

    Created button add

---
+++ introAReact/fitness/src/components/Add.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import imageAdd from '../images/add.png'
+
+const Add = () =>(
+ 
+        <div className = "add mx-auto Add-Button">
+            <img
+                src={imageAdd}
+            />
+        </div>
+    
+)
+
+export default Add
--- introAReact/fitness/src/index.js
+++ introAReact/fitness/src/index.js
@@ -6,15 +6,11 @@ container.appendChild(element)*/
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import Welcome from './components/Welcome'
+import Exercises from './pages/Exercises'
 import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root')
 
 //ReactDOM.render(__QueElemento__, __Donde__)
 
-ReactDOM.render(<Card 
-                    description="Learn amazing street workout and calisthenics"
-                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"    
-                    leftColor="#A74CF2"
-                    rightColor="#617BFB"
-                    title="Technique Guides"
-                />, container)
+ReactDOM.render(<Exercises/>, container)
