3f85b9c5a5f7b106d8414555ea17f26cbf3975b4
EzzeGuason <ezequieledjuarez>
2021-01-18 20:11:35 -0300

    Components controlled

--- introAReact/fitness/src/pages/ExerciseNew.js
+++ introAReact/fitness/src/pages/ExerciseNew.js
@@ -1,49 +1,77 @@
 import React from 'react'
 
 class ExerciseNew extends React.Component{
+    
+    state = {}
 
-    handleClick= () =>{
-        console.log('Clicked')
+    handleSubmit = e =>{
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    handleChange = e =>{
+        this.setState({
+            [e.target.name] : e.target.value
+        })
     }
 
     render(){
         return(
            <div className = "container">
-               <form>
+               <form
+                    onSubmit = { this.handleSubmit}
+               >
                    <div className = "form-group">
                        <input
                             className = "form-control"
+                            name = "title"
+                            onChange = { this.handleChange }
                             placeholder = "title"
-                            type = "email"
+                            type = "text"
+                            value={this.state.title}
                        />
                    </div>
                    <div className = "form-group">
                        <input
                             className = "form-control"
+                            name = "description"
+                            onChange = {this.handleChange}
                             placeholder = "description"
                             type = "text"
+                            value={this.state.description}
                        />
                     </div>
                     <div className = "form-group">
                        <input
                             className = "form-control"
+                            name = "img"
+                            onChange = {this.handleChange}
                             placeholder = "img"
                             type = "text"
+                            value={this.state.img}
                        />
                    </div>
                    <div className = "form-row">
                        <div className = "col">
                             <input
                                 className = "form-control"
-                                type = "text"
+                                name = "leftColor"
+                                onChange = {this.handleChange}
                                 placeholder = "leftColor"
+                                type = "text"
+                                value={this.state.leftColor}
+                                
                             />
                        </div>
                        <div className = "col">
                             <input
                                 className = "form-control"
-                                type = "text"
+                                name = "rightColor"
+                                onChange = {this.handleChange}
                                 placeholder = "rightColor"
+                                type = "text"
+                                value={this.state.rightColor}
+                                
                             />
                        </div>
                     </div>
